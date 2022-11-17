e227648ce2c5890dcc4fdbf0aef4d4253bec34e3
VeraBu <Opilki-in-the-head@mail.ru>
2022-11-17 10:03:54 +0300

    added task 13_1

---
+++ Task_13_1/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Введите число N: ");
+string inputNumber = Console.ReadLine() ?? "";
+
+int numberN = int.Parse(inputNumber);
+string resDescrip;
+int result;
+
+if (numberN < 100)
+{
+    resDescrip = "третьей цифры нет";
+}
+else
+{
+    while (numberN > 999)
+    {
+        numberN = numberN / 10;
+    }
+result = numberN % 10;
+resDescrip= $"{inputNumber} -> {result}";
+}
+
+Console.WriteLine(resDescrip);
