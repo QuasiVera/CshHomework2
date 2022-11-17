e7f9f16d5b5848c1a5c655f3a5e8d4d71628d772
VeraBu <Opilki-in-the-head@mail.ru>
2022-11-17 08:03:43 +0300

    added task 10

---
+++ Task_10/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 10. Напишите программу, которая принимает на вход трехзначное число и на выходе 
+//показывает вторую цифру этого числа
+int numberA;
+
+while (true)
+{
+    Console.Write("Введите трехзначное число: ");
+    if (int.TryParse(Console.ReadLine(), out numberA) & numberA<999 & numberA>99)
+        break;
+    Console.WriteLine("Ошибка ввода");
+}
+
+int result = (numberA/10)%10;
+
+Console.WriteLine($"{numberA} -> {result}");
